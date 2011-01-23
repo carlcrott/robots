3e5c98b1b6d9b2e7d987e1d2809a1b5bf83765e2
Carl Crott <carlcrott@gmail.com>
2011-01-22 21:44:30 -0500

    added a bunch of stuff

---
+++ app/controllers/ssh_connections_controller.rb
@@ -0,0 +1,12 @@
+require 'net/ssh'
+require 'logger'
+
+Net::SSH.start(
+  'host', 'user',
+  :host_key => "ssh-rsa",
+  :encryption => "blowfish-cbc",
+  :keys => [ "/tmp/temporary-key" ],
+  :compression => "zlib"
+) do |session|
+  ...
+end
