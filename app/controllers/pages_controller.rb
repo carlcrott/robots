489b51ea9ff40d047ee018100199a41c0c2cbd0a
Carl Crott <carlcrott@gmail.com>
2011-01-23 14:00:41 -0500

    finished w controller .. request runs sucessfully

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,39 @@
+require 'net/ssh'
+require 'logger'
+
 class PagesController < ApplicationController
   def home
     @title = " HOME PAGE DAR "
   end
+  
+  def foo
+    render :text => 'bar'
+  end
+  
+  
+  
+  def conn
+  
+  Net::SSH.start( '127.0.0.1','unf-ubu' ) do |session|
+    session.open_channel do |channel|
+      channel.on_close do |ch|
+        puts "channel closed successfully."
+        render :text => 'hits'
+      end
+      puts "closing channel..."
+      channel.close
+    end
+
+    session.loop
+  end
+    
+  end
+
+
+
+
+
+
+
 
 end
